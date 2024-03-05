ae873b51ad2cd1a5786e4945f7bdc7ffd73b9725
cwdatlas <ascott@carroll.edu>
2024-03-04 23:52:18 -0700

    added @WithTransation to functions accessing the database

--- src/main/kotlin/services/LogService.kt
+++ src/main/kotlin/services/LogService.kt
@@ -1,5 +1,6 @@
 package services
 
+import io.quarkus.hibernate.reactive.panache.common.WithTransaction
 import io.quarkus.panache.common.Sort
 import io.smallrye.mutiny.Uni
 import jakarta.enterprise.context.ApplicationScoped
@@ -12,13 +13,15 @@ class LogService {
     @Inject
     lateinit var logRepo: LogRepo
 
+    @WithTransaction
     fun listAll(): Uni<List<HorizonLog>>{
         return logRepo.listAll(Sort.by("date"))
     }
-
+    @WithTransaction
     fun persist(log: HorizonLog): Uni<HorizonLog> {
         return logRepo.persist(log)
     }
+    @WithTransaction
     fun deleteByID(id: Long): Uni<Boolean> {
         return logRepo.deleteById(id)
     }
